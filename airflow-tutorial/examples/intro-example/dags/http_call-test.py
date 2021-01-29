7d1618630f3b42742a27540435c4a39f64f99f08
Avinash Singh Kachhwaha <singhavinash857@gmail.com>
2021-01-29 20:33:01 +0530

    adding rest end point calls

--- airflow-tutorial/examples/intro-example/dags/http_call-test.py
+++ airflow-tutorial/examples/intro-example/dags/http_call-test.py
@@ -8,6 +8,8 @@ from airflow.operators.http_operator import SimpleHttpOperator
 from airflow.operators.sensors import HttpSensor
 from airflow.utils.dates import days_ago
 from airflow.operators.python_operator import PythonOperator
+from airflow.models import Variable
+
 
 default_args = {
     'owner': 'airflow',
@@ -31,16 +33,66 @@ dag.doc_md = __doc__
 def transform_json(**kwargs):
     ti = kwargs['ti']
     pulled_value_1 = ti.xcom_pull(key=None, task_ids='get_data')
-    print(pulled_value_1)
+    import_body= json.loads(pulled_value_1)
+    print(import_body)
+   
+
+
     # transform the json here and save the content to a file
 
-task_get_data = SimpleHttpOperator(
-    task_id='get_data',
+def save_emp_json(**kwargs):
+    ti = kwargs['ti']
+    employee = ti.xcom_pull(key=None, task_ids='save_employee')
+    import_body= json.loads(employee)
+    print(import_body)
+    print(import_body["id"])
+    print(import_body["name"])
+    id =   import_body["id"]
+    print(id)
+    empid = id
+    print(empid)
+
+    Variable.set("id", empid)
+    print(Variable.get("id"))
+
+
+    # transform the json here and save the content to a file
+
+task_save_employee = SimpleHttpOperator(
+    task_id='save_employee',
     http_conn_id='atlassian_marketplace',
     # endpoint='/rest/2',
-    endpoint='/test-data',
+    endpoint='/save',
+    method="POST",
+    data=json.dumps({"name": "avinash singh"}),
+    # data="{\"name\" : \"Avinash Singh\"}",
+    headers={"Content-Type": "application/json"},
+    response_filter=lambda response: response.json(),
+    xcom_push=True,
+    dag=dag,
+)
+
+
+task_get_all_employee = SimpleHttpOperator(
+    task_id='get_all_employee',
+    http_conn_id='atlassian_marketplace',
+    # endpoint='/rest/2',
+    endpoint='/get-all',
     method="GET",
-    headers={"Content-Type": "application/x-www-form-urlencoded"},
+    headers={"Content-Type": "application/json"},
+    response_filter=lambda response: response.json(),
+    xcom_push=True,
+    dag=dag,
+)
+
+
+task_get_byid_employee = SimpleHttpOperator(
+    task_id='get-by-id-employee',
+    http_conn_id='atlassian_marketplace',
+    # endpoint='/rest/2',
+    endpoint="/get-by-id/{empId}/employee".format(empId =  Variable.get("id")),
+    method="GET",
+    headers={"Content-Type": "application/json"},
     response_filter=lambda response: response.json(),
     xcom_push=True,
     dag=dag,
@@ -51,7 +103,7 @@ task_get_data = SimpleHttpOperator(
 task_http_sensor_check = HttpSensor(
     task_id='http_sensor_check',
     http_conn_id='atlassian_marketplace',
-    endpoint='',
+    endpoint='/',
     request_params={},
     # response_check=lambda response: "httpbin" in response.text,
     poke_interval=5,
@@ -67,4 +119,11 @@ save_and_transform = PythonOperator(
     provide_context=True,
 )
 
-task_http_sensor_check >> task_get_data >> save_and_transform
+save_employee = PythonOperator(
+    task_id="save_employee_transform", 
+    python_callable=save_emp_json,
+    provide_context=True,
+)
+
+task_http_sensor_check >> task_save_employee >> task_get_all_employee >> task_get_byid_employee >> save_and_transform
+task_save_employee >> save_employee
