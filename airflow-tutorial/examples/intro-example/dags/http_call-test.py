5c413f462618b97cb10e7ac8f86292311361eb29
Avinash Singh Kachhwaha <singhavinash857@gmail.com>
2021-02-03 16:27:28 +0530

    adding slack amd mail notification

--- airflow-tutorial/examples/intro-example/dags/http_call-test.py
+++ airflow-tutorial/examples/intro-example/dags/http_call-test.py
@@ -11,13 +11,85 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.models import Variable
 
 
+from airflow.hooks.base_hook import BaseHook
+from airflow.contrib.operators.slack_webhook_operator import SlackWebhookOperator
+
+SLACK_CONN_ID = 'slack-notification'
+def task_fail_slack_alert(context):
+    slack_webhook_token = BaseHook.get_connection(SLACK_CONN_ID).password
+    slack_msg = """
+            :red_circle: Task Failed. 
+            *Task*: {task}  
+            *Dag*: {dag} 
+            *Execution Time*: {exec_date}  
+            *Log Url*: {log_url} 
+            """.format(
+            task=context.get('task_instance').task_id,
+            dag=context.get('task_instance').dag_id,
+            ti=context.get('task_instance'),
+            exec_date=context.get('execution_date'),
+            log_url=context.get('task_instance').log_url,
+        )
+    failed_alert = SlackWebhookOperator(
+        task_id='slack_test',
+        http_conn_id='slack-notification',
+        webhook_token=slack_webhook_token,
+        message=slack_msg,
+        username='airflow')
+    return failed_alert.execute(context=context)
+
 def callable_func(context):
     print(context)
     dag_id = context.get('task_instance').dag_id
     task_id = context.get('task_instance').task_id
+    log_url = context.get('task_instance').log_url
+    log_filepath = context.get('task_instance').log_filepath
+    exec_date = context.get('execution_date')
 
-    email_title = "Airflow Task {task_id} Success".format(task_id = task_id)
-    email_body = "{task_id} in {dag_id} Success.".format(task_id = task_id, dag_id = dag_id)
+    email_title = """
+    Airflow Alert-- Airflow Task {task_id} is Success
+    """.format(task_id = task_id)
+
+    email_body = """
+    <table class="reportWrapperTable" cellspacing="4" cellpadding="2" width="100%" rules="rows" style="border-collapse:collapse;color:#1f2240;background-color:#ffffff">
+    <caption style="background-color:#ffffff;color:#1f2240;margin-bottom:.5em;font-size:18pt;width:100%;border:0">Airflow Alert</caption>
+    <thead style="100%;color:#ffffff;background-color:#1f2240;font-weight:bold">
+    <tr>
+    <th scope="col" style="background-color:#1f2240">Name</th>
+    <th scope="col" style="background-color:#1f2240">Status</th>
+    </tr>
+    </thead>
+    <tbody>
+
+    <tr>
+    <td>The Task: </td>
+    <td>{task_id}</td>
+    </tr>
+    
+    <tr>
+    <td>The Dag: </td>
+    <td>{dag_id}</td>
+    </tr>
+
+    <tr>
+    <td>Execution Time: </td>
+    <td>{exec_date}</td>
+    </tr>
+
+    <tr>
+    <td>Status: </td>
+    <td> <span style="font-size: 20px; color: #008000;">
+    Success.
+    </span>
+    </td>
+    </tr>
+
+    <tr>
+    <td>Log Url: </td>
+    <td><a href="{log_url}">Link</a></td>
+    </tr>
+    </tbody></table>
+    """.format(task_id = task_id, dag_id = dag_id,log_url=log_url,exec_date=exec_date)
     to = "avinash.kachhwaha@oracle.com"
     send_email(to, email_title, email_body)
 
@@ -25,9 +97,18 @@ def error_callable_func(context):
     print(context)
     dag_id = context.get('task_instance').dag_id
     task_id = context.get('task_instance').task_id
+    log_url = context.get('task_instance').log_url
 
-    email_title = "Airflow Task {task_id} Failed".format(task_id = task_id)
-    email_body = "{task_id} in {dag_id} Failed.".format(task_id = task_id, dag_id = dag_id)
+    email_title = "Airflow Alert-- Airflow Task {task_id} Failed".format(task_id = task_id)
+    email_body = """
+    The Task: {task_id} <br>
+    The Dag: {dag_id} <br>
+    Status: 
+    <span style="font-size: 20px; color: #FF0000;">
+    Failed.
+    </span>
+    <br>Log Url: <a href="{log_url}">Link</a><br>
+    """.format(task_id = task_id, dag_id = dag_id,log_url=log_url)
     to = "avinash.kachhwaha@oracle.com"
     send_email(to, email_title, email_body)
     
@@ -41,7 +122,9 @@ default_args = {
     'retry_delay': timedelta(minutes=1),
     'provide_context': True,
     'on_success_callback': callable_func,
-    'on_failure_callback' : error_callable_func
+    'on_failure_callback' : task_fail_slack_alert
+    # 'on_failure_callback' : error_callable_func
+
 }
 
 dag = DAG(
